dc9a1c35776e6bda3587925f371ef66322a8ef07
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-07-27 12:35:50 +0500

    class componentlarni o'rganib olish kerak bo'ladi

---
+++ 4. Anything project/argument.js
@@ -0,0 +1,60 @@
+// function Polygon(height, width) {
+//   this.name = "Polygon";
+//   this.height = height;
+//   this.width = width;
+// }
+
+// // method prototype
+
+// Polygon.prototype.sayName = function () {
+//   console.log("Polygonmi: " , this.name);
+// };
+// // -----------------
+
+// let polygon = new Polygon(10, 20);
+
+// polygon.sayName();
+
+// class Polygon {
+//   constructor(height, withd) {
+//     this.name = "Polygon";
+//     this.height = height;
+//     this.withd = withd;
+//   }
+//   sayName() {
+//     console.log("Polygonmi: ", this.name, this.height, this.withd);
+//   }
+// }
+
+// class Square extends Polygon {
+//   constructor(length) {
+//     super(length, length);
+//     this.name = "Square";
+//   }
+//   get area() {
+//     return this.height * this.withd;
+//   }
+//   set area(value) {
+//     this.area = value;
+//   }
+// }
+
+// let  s = new Square(5)
+// s.sayName()
+
+// console.log(s.area);
+
+function Component(name) {
+  this.name = name;
+}
+
+Component.prototype.sayName = function () {
+  console.log("Component: ", this.name);
+  let outputElement = document.getElementById("output");
+  outputElement.innerHTML = 'Component: ' + this.name;
+};
+
+let comp = new Component("Salom dunyo");
+
+comp.sayName();
+
