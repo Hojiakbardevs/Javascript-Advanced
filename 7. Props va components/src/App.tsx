ac7f75b37e7ccfbca14861d8a60a1a5bc011ba0d
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-08-05 19:18:18 +0500

    downloaded react vite app

---
+++ 7. Props va components/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+      <div>
+          <h1>Component bilan hooklarni ishlashni o'rganamiz</h1>
+          
+      </div>
+    </>
+  );
+}
+
+export default App;
