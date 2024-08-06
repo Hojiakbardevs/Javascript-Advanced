d3c61908ff5cc6624346ba19901792d7cd11d5ad
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-08-06 20:13:51 +0500

    bu testlar xato edi yana o'rganaveramiz

--- 7. Props va components/src/App.tsx
+++ 7. Props va components/src/App.tsx
@@ -1,13 +1,9 @@
-import  Avatar  from "./components/avatar";
-
 function App() {
-  const name = "Micheal Jackson"
-  const description = "https://reqres.in/img/faces/9-image.jpg"
   return (
     <>
       <div>
         <h1>Component bilan hooklarni ishlashni o'rganamiz</h1>
-        <Avatar name={name} description={description}></Avatar>
+        {/* <Avatar name={name} description={description}></Avatar> */}
       </div>
     </>
   );
