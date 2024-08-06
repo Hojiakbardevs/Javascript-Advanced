04b48e11e073fa248feb52fac80b81a7cd6c9ce1
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-08-06 19:42:02 +0500

    samigjonov akanikini bir sinab o'rganib olay

--- 7. Props va components/src/components/avatar.tsx
+++ 7. Props va components/src/components/avatar.tsx
@@ -1,12 +1,21 @@
 
-const Avatar = ({name, description}) => {
-    return (
-      <div>
-      <h2>Avatar component</h2>
-      <img src={description}  alt="img" />
-      <p>{name}</p>
-  </div>
-    )
-  }
+// const Avatar = ({name, description}) => {
+//     return (
+//       <div>
+//       <h2>Avatar component</h2>
+//       <img src={description}  alt="img" />
+//       <p>{name}</p>
+//   </div>
+//     )
+//   }
   
-  export default Avatar
+//   export default Avatar
+
+import { ReactNode } from "react";
+
+interface Iprops {
+  render:(name: string, description: string) => ReactNode
+}
+const Avatar = ({render}: Iprops) => render;
+
+export default Avatar
