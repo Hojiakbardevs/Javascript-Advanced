2d31fd6803222405da78902abf53450a25007d3d
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-08-26 14:02:24 +0500

    ikkinchi darajali kod yozdim men

--- 22. Counter/script.js
+++ 22. Counter/script.js
@@ -1,21 +1,40 @@
-const countrDisplay = document.getElementById("counter-value");
+// const countrDisplay = document.getElementById("counter-value");
+// let counter = 0;
+
+// function incrementCounter() {
+//   counter++;
+//   countrDisplay.textContent = counter;
+// }
+
+// function decrementCounter() {
+//   counter--;
+//   countrDisplay.textContent = counter;
+// }
+
+// function resetCounter() {
+//   counter = 0;
+//   countrDisplay.textContent = counter;
+// }
+
+// document.getElementById("increase-btn").addEventListener("click", incrementCounter);
+// document.getElementById("decrease-btn").addEventListener("click", decrementCounter);
+// document.getElementById("reset-btn").addEventListener("click", resetCounter);
+
+const counterDisplay = document.getElementById("counter-value");
+
 let counter = 0;
 
-function incrementCounter() {
-  counter++;
-  countrDisplay.textContent = counter;
-}
-
-function decrementCounter() {
-  counter--;
-  countrDisplay.textContent = counter;
-}
-
-function resetCounter() {
-  counter = 0;
-  countrDisplay.textContent = counter;
-}
-
-document.getElementById("increase-btn").addEventListener("click", incrementCounter);
-document.getElementById("decrease-btn").addEventListener("click", decrementCounter);
-document.getElementById("reset-btn").addEventListener("click", resetCounter);
+document.querySelectorAll(".btn").forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const action = event.currentTarget.getAttribute("data-action");
+    if(action === 'increase'){
+        counter++;
+    } else if (action === 'decrease') {
+      counter--;
+    }
+    else {
+      counter = 0;
+    }
+    counterDisplay.textContent = counter;
+  });
+});
